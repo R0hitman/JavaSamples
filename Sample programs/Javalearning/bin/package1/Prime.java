162ad80b8ccfbeea60d845db9b76f3e304404378
rohit bhatkulkar <rohit.bhatkulkar@amla.io>
2019-04-10 11:57:29 +0530

    Prime
    Prime Number

---
+++ Sample programs/Javalearning/bin/package1/Prime.java
@@ -0,0 +1,33 @@
+public class PrimeNumber{
+
+	public static void main(String[] args) 
+{
+		// TODO Auto-generated method stub
+
+		
+		for(int i=0;i<=100;i++)
+	{
+
+		For (j=2;j<=i-1;j++)
+		{
+
+
+		if(i%j==0)
+			{
+			System.out.print("Number is not prime");
+
+			}
+		else()
+			{
+		
+			System.out.print("Number is a prime"); 
+
+			}
+
+		}
+	
+		
+
+	}
+
+}
