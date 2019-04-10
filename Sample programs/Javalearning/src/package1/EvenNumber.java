d64d5f6ec598f87e352dcd1159db7db6d7b794c0
R0hitman <48086216+R0hitman@users.noreply.github.com>
2019-04-10 09:00:44 +0530

    Even Number
    To print even number between 1 to 100

---
+++ Sample programs/Javalearning/src/package1/EvenNumber.java
@@ -0,0 +1,26 @@
+package package1;
+
+public class EvenNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		for(int i=0;i<=100;i++){
+			
+			if(i%2==0){
+				
+				System.out.println(i);
+			}
+			
+			
+			
+		}
+		
+		
+		
+	}
+	
+	
+
+}
