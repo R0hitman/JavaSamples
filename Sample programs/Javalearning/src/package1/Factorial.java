c01cf8cc62b204b6aafcec83dd8b3bc907c9a096
R0hitman <48086216+R0hitman@users.noreply.github.com>
2019-04-14 21:59:47 +0530

    factorial

---
+++ Sample programs/Javalearning/src/package1/Factorial.java
@@ -0,0 +1,18 @@
+package package1;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n=5,i,fact=1;
+		
+		for(i=1;i<=n;i++){
+			
+			fact=fact*i;
+			
+		}
+		System.out.println(fact);
+	}
+
+}
