0192ac2e3a3a3424dab32604fabeae09dfce0d33
R0hitman <48086216+R0hitman@users.noreply.github.com>
2019-04-17 09:15:18 +0530

    Sum of cube from 1 to 10

---
+++ Sample programs/Javalearning/src/package1/CubeSeries.java
@@ -0,0 +1,15 @@
+package package1;
+
+public class CubeSeries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int sum=0,i;
+		for(i=0;i<=10;i++){
+			sum=(sum+i*i*i);
+		}
+		System.out.print(sum);
+	}
+
+}
