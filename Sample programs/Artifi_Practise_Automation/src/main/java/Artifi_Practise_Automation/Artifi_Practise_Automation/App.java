c6c1ceac8f230417d410200c1cf2703485dfc3f8
R0hitman <48086216+R0hitman@users.noreply.github.com>
2019-05-02 08:35:12 +0530

    Artifi_Maven_Automation

---
+++ Sample programs/Artifi_Practise_Automation/src/main/java/Artifi_Practise_Automation/Artifi_Practise_Automation/App.java
@@ -0,0 +1,13 @@
+package Artifi_Practise_Automation.Artifi_Practise_Automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
