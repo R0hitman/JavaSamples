549003c9c3949d3cd8c5955b12849990b65fdae2
rohit bhatkulkar <rohit.bhatkulkar@amla.io>
2019-05-07 11:12:48 +0530

    QA

---
+++ src/main/java/Artifi_ProductSetup/Artifi_ProductSetup/App.java
@@ -0,0 +1,13 @@
+package Artifi_ProductSetup.Artifi_ProductSetup;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
